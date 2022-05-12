274e4d769d7ebb3c9e3b63e08ca3c5466094fc95
amolu <amolujagare@gmail.com>
2022-05-12 09:01:43 +0530

    POM cucumber

--- src/main/java/pages/DarkskyHomePage.java
+++ src/main/java/pages/DarkskyHomePage.java
@@ -2,10 +2,36 @@ package pages;
 
 import org.openqa.selenium.By;
 
+import java.util.ArrayList;
+
 public class DarkskyHomePage extends Base {
 
     By currentTemp = By.xpath("//span[@class='summary swap']");
     By timelineTemp = By.xpath("//span[@class='first']//span");
+    By timeListRaw = By.xpath("//span[@class='hour']/span");
+
+    public ArrayList<Integer> getTimeList()
+    {
+        ArrayList<String> timeListStr = getElementTextList(timeListRaw);
+        System.out.println(timeListStr);
+
+        ArrayList<Integer> timeList= new ArrayList<>();
+
+        for (int i=0;i<timeListStr.size();i++)
+        {
+           String rawTimeStr = timeListStr.get(i);  // 10am 0r 2pm
+            int l = rawTimeStr.length();
+
+            String timeStr = rawTimeStr.substring(0, l - 2); // 10 or 2
+
+            int time = Integer.parseInt(timeStr);
+
+            timeList.add(time);
+        }
+        System.out.println(timeList);
+
+        return timeList;
+    }
 
     public String getTimelineTemp()
     {
--- src/main/java/stepdefinitions/DarkskySD.java
+++ src/main/java/stepdefinitions/DarkskySD.java
@@ -5,11 +5,20 @@ import io.cucumber.java.en.Then;
 import org.junit.Assert;
 import pages.DarkskyHomePage;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import static stepdefinitions.SharedSD.getDriver;
+
 public class DarkskySD {
 
     DarkskyHomePage darkskyHomePage = new DarkskyHomePage();
     @Given("I am on Darksky Home Page")
     public void i_am_on_darksky_home_page() {
+
+        Assert.assertEquals("This is not a darksky page",
+                "Dark Sky - Sansad Marg, New Delhi, Delhi",
+        getDriver().getTitle());
 
           }
     @Then("I verify current temp is equal to Temperature from Daily Timeline")
@@ -26,4 +35,45 @@ public class DarkskySD {
 
     }
 
+    @Then("I verify timeline is displayed with two hours incremented")
+    public void iVerifyTimelineIsDisplayedWithTwoHoursIncremented() {
+        ArrayList<Integer> timeList = darkskyHomePage.getTimeList();
+
+        // [10, 12, 2, 4, 6, 8, 10, 12, 2, 4, 6] --> 11
+        ArrayList<Integer> timeDiffList = new ArrayList<>();
+
+        for (int i = 0; i < timeList.size()-1; i++) // 11 times
+        {
+            int time1 = timeList.get(i);
+            int time2 = timeList.get(i+1);
+
+            int timeDiff = 0;
+
+            if(time2>time1)
+                timeDiff = time2 - time1;
+
+            if(time2<time1)
+                timeDiff = (time2+12) - time1;
+
+            timeDiffList.add(timeDiff);
+        }
+
+        System.out.println(timeDiffList);
+
+       /* boolean flag = true;
+
+        for (int i=0;i<timeDiffList.size();i++)
+        {
+            if(timeDiffList.get(i)!=2)
+                flag=false;
+        }*/
+
+        int size = timeDiffList.size();
+        int occurance = Collections.frequency(timeDiffList,2);
+
+        boolean flag = (size == occurance);
+
+        Assert.assertTrue("All differences are not 2",flag);
+
+    }
 }
